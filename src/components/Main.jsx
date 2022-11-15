393a667b67c59d5714f50dcdffad6796a73e8d63
Hritvik Mohan <hritvik5005@gmail.com>
2022-11-15 07:03:21 +0530

    Add Main and Navbar Components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,19 @@
+import reactjsicon from "../assets/reactjsicon.png"
+
 export default function Main() {
-    return <h1>Main goes here</h1>
+    return (
+    <main>
+        <div className="main">
+            <h1 className="main-heading">Fun facts about React</h1>
+            <ul className="list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            {/* <img className="reactjsicon" src={reactjsicon} alt="" /> */}
+        </div>
+    </main>
+    )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,21 @@
+import logo from "../assets/react.svg"
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
+
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>
+    return (
+    <nav className="">
+        <div className="nav">
+            <div>
+                <img src={logo} alt="react logo"/>
+                <h1>ReactFacts</h1>
+            </div>
+            <p>React Course - Project 1</p>
+        </div>
+    </nav>
+    )
 }
